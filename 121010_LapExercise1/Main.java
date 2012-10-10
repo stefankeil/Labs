695345587c299a835a34786b93b72434b784b972
Fee <fee.braun@hotmail.com>
2012-10-10 22:51:35 +0200

    Übersichtlicheres Menü

--- 121010_LapExercise1/Main.java
+++ 121010_LapExercise1/Main.java
@@ -5,66 +5,42 @@ public class Main {
 	public static void main(String[] args) {
 		Cinema hall = new Cinema();
 		int bookingDecision = 0;
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
 		while (bookingDecision != 4) {
 			System.out.println();
-			System.out
-					.println("Was möchten Sie tun? \n 1 = Tickets buchen  \n 2 = Aktuelle Belegung anzeigen \n 3 = Buchung löschen \n 4 = Programm beenden");
+			System.out.println("Was möchten Sie tun? \n 1 = Tickets buchen  \n 2 = Aktuelle Belegung anzeigen \n 3 = Buchung löschen \n 4 = Programm beenden");
 			System.out.print("> ");
 			try {
-				BufferedReader in = new BufferedReader(new InputStreamReader(
-						System.in));
 				bookingDecision = Integer.parseInt(in.readLine());
 
 				switch (bookingDecision) {
-					case 1:
-						System.out
-								.println("Bitte geben Sie die Anzahl der Tickets ein, die Sie buchen möchten./n Der Preis pro Ticket ist: "+ Cinema.getTicketPrice()+" Euro");
-						System.out.print("> ");
-						try {
-							BufferedReader anzahl = new BufferedReader(
-									new InputStreamReader(System.in));
-							int ticketCount = Integer.parseInt(anzahl.readLine());
-							hall.book(ticketCount);
-						} catch (IOException ex) {
-							System.out.println(ex.getMessage());
-						}
-						
-						break;
-					case 2:
-						hall.printCinema();
-						break;
-					case 3:
-						System.out
-								.println("Bitte geben Sie die Buchungsnummer Ihrer Buchung ein.");
-						System.out.print("> ");
-						try {
-							BufferedReader delete = new BufferedReader(
-									new InputStreamReader(System.in));
-							int bookingNumber = Integer.parseInt(delete.readLine());
-							hall.deleteBooking(bookingNumber);
-						} catch (IOException ex) {
-							System.out.println(ex.getMessage());
-						}
-						break;
-					case 4:
-						System.out.println("Auf Wiedersehen!");
-						break;
-	
-					default:
-						System.out
-								.println("Wir konnten Ihre Eingabe nicht erkennen.");
-						break;
-					}
+				case 1:
+					System.out.println("Bitte geben Sie die Anzahl der Tickets ein, die Sie buchen möchten./n Der Preis pro Ticket ist: " + Cinema.getTicketPrice() + " Euro");
+					System.out.print("> ");
+					int ticketCount = Integer.parseInt(in.readLine());
+					hall.book(ticketCount);
+					break;
+				case 2:
+					hall.printCinema();
+					break;
+				case 3:
+					System.out.println("Bitte geben Sie die Buchungsnummer Ihrer Buchung ein.");
+					System.out.print("> ");
+					int bookingNumber = Integer.parseInt(in.readLine());
+					hall.deleteBooking(bookingNumber);
+					break;
+				case 4:
+					System.out.println("Auf Wiedersehen!");
+					break;
+
+				default:
+					System.out.println("Wir konnten Ihre Eingabe nicht erkennen.");
+					break;
+				}
 			} catch (IOException ex) {
 				System.out.println(ex.getMessage());
 			}
-
-			/*
-			 * hall.book(4); hall.book(5); hall.book(1); hall.book(1);
-			 */
-
-			// hall.deleteBooking(101);
 		}
 	}
 }
