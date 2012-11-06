d310c9f3a331c6dcd77d11383f0917ced643c910
Fee <fee.braun@hotmail.com>
2012-11-06 20:40:15 +0100

    2. Aufgabe

--- 121106_LabExercise5/src/Main.java
+++ 121106_LabExercise5/src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
@@ -5,8 +8,32 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		JulianDate test = new JulianDate();
-		test.convertGregoriantoJulian();
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println("Geben Sie den Tag an dem Sie geboren sind ein: ");
+			int day = Integer.parseInt(in.readLine());
+			System.out.println("Geben Sie den Monat an dem Sie geboren sind ein: ");
+			int month = Integer.parseInt(in.readLine());
+			System.out.println("Geben Sie das Jahr an dem Sie geboren sind ein: ");
+			int year = Integer.parseInt(in.readLine());
+			
+			JulianDate test = new JulianDate();
+
+			test.day = day;
+			test.month = month;
+			test.year = year;
+			test.printGregorianCalendarDate();
+			test.getDaysBetween(test.convertGregoriantoJulian(), test.printGregorianCalendarDate());
+			test.isEqual(test.day, test.actualDay(), test.month, test.actualMonth());
+			test.getWeekDay(test.convertGregoriantoJulian());
+
+
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
