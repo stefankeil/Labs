28eaffe58649a808eb86666bad48bd8675f47f73
Fee <fee.braun@hotmail.com>
2012-11-21 10:52:26 +0100

    UnderflowExecption hinzugefügt

--- 121120_LabExercise7/src/Stack.java
+++ 121120_LabExercise7/src/Stack.java
@@ -1,35 +1,37 @@
-
 public class Stack<T> {
-	
+
 	private LinkedList<T> postfixStack;
-	
-	public Stack(){
+
+	public Stack() {
 		postfixStack = new LinkedList<T>();
 	}
-	
-	public void push(T item){
+
+	public void push(T item) {
 		postfixStack.insert(item);
 	}
-	
-	public LinkedListItem<T> pop(){
-		if (!empty()){
-		return postfixStack.delete();
-		}else{
-			return null;
-		}
-	}
-	
-	public boolean empty(){
+
+	public boolean empty() {
 		return postfixStack.isEmpty();
 	}
-	
-	public void print(){
+
+	public void print() {
 		postfixStack.print();
 	}
-	
-	public void top(){
-		postfixStack.top();
+
+	public LinkedListItem<T> pop() throws UnderflowException {
+		if (!empty()) {
+			return postfixStack.delete();
+		} else {
+			throw new UnderflowException("ListStack pop");
+		}
 	}
-	
-	
+
+	public void top() throws UnderflowException {
+		if (!empty()) {
+			postfixStack.top();
+		} else {
+			throw new UnderflowException("Stack Underflow");
+		}
+	}
+
 }
