bfd0f62e7c46a1cd876ace44ed896d7d4f708e27
Fee <fee.braun@hotmail.com>
2012-12-11 21:38:23 +0100

    First Try for recursion

--- 121211_LabExercise10/src/Canvas.java
+++ 121211_LabExercise10/src/Canvas.java
@@ -13,7 +13,6 @@ public class Canvas {
 	private List objects;
 	private HashMap shapes;
 
-
 	Canvas(int width, int height, Color bgColour) {
 		frame = new JFrame();
 		canvas = new CanvasPane();
@@ -26,19 +25,8 @@ public class Canvas {
 		setVisible(true);
 	}
 
-	/**
-	 * Set the canvas visibility and brings canvas to the front of screen when
-	 * made visible. This method can also be used to bring an already visible
-	 * canvas to the front of other windows.
-	 * 
-	 * @param visible
-	 *            boolean value representing the desired visibility of the
-	 *            canvas (true or false)
-	 */
 	public void setVisible(boolean visible) {
 		if (graphic == null) {
-			// first time: instantiate the offscreen image and fill it with
-			// the background colour
 			Dimension size = canvas.getSize();
 			canvasImage = canvas.createImage(size.width, size.height);
 			graphic = (Graphics2D) canvasImage.getGraphics();
@@ -49,44 +37,12 @@ public class Canvas {
 		frame.setVisible(visible);
 	}
 
-	/**
-	 * Draw a given shape onto the canvas.
-	 * 
-	 * @param referenceObject
-	 *            an object to define identity for this shape
-	 * @param color
-	 *            the color of the shape
-	 * @param shape
-	 *            the shape object to be drawn on the canvas
-	 */
-	// Note: this is a slightly backwards way of maintaining the shape
-	// objects. It is carefully designed to keep the visible shape interfaces
-	// in this project clean and simple for educational purposes.
 	public void draw(Object referenceObject, String color, Shape shape) {
-		//objects.remove(referenceObject); // just in case it was already there
 		objects.add(referenceObject); // add at the end
 		shapes.put(referenceObject, new ShapeDescription(shape, color));
 		redraw();
 	}
 
-	/**
-	 * Erase a given shape's from the screen.
-	 * 
-	 * @param referenceObject
-	 *            the shape object to be erased
-	 */
-//	public void erase(Object referenceObject) {
-//		objects.remove(referenceObject); // just in case it was already there
-//		shapes.remove(referenceObject);
-//		redraw();
-//	}
-
-	/**
-	 * Set the foreground colour of the Canvas.
-	 * 
-	 * @param newColour
-	 *            the new colour for the foreground of the Canvas
-	 */
 	public void setForegroundColor(String colorString) {
 		if (colorString.equals("red"))
 			graphic.setColor(Color.red);
@@ -106,21 +62,13 @@ public class Canvas {
 			graphic.setColor(Color.black);
 	}
 
-
-//	/**
-//	 * Redraw ell shapes currently on the Canvas.
-//	 */
 	private void redraw() {
-		//erase();
 		for (Iterator i = objects.iterator(); i.hasNext();) {
 			((ShapeDescription) shapes.get(i.next())).draw(graphic);
 		}
 		canvas.repaint();
 	}
 
-//	/**
-//	 * Erase the whole canvas. (Does not repaint.)
-//	 */
 	private void erase() {
 		Color original = graphic.getColor();
 		graphic.setColor(backgroundColour);
@@ -129,22 +77,12 @@ public class Canvas {
 		graphic.setColor(original);
 	}
 
-	/************************************************************************
-	 * Inner class CanvasPane - the actual canvas component contained in the
-	 * Canvas frame. This is essentially a JPanel with added capability to
-	 * refresh the image drawn on it.
-	 */
 	private class CanvasPane extends JPanel {
 		public void paint(Graphics g) {
 			g.drawImage(canvasImage, 0, 0, null);
 		}
 	}
 
-	/************************************************************************
-	 * Inner class CanvasPane - the actual canvas component contained in the
-	 * Canvas frame. This is essentially a JPanel with added capability to
-	 * refresh the image drawn on it.
-	 */
 	private class ShapeDescription {
 		private Shape shape;
 		private String colorString;
