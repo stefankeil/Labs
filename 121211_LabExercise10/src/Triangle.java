aae0a65e00ebc80381c1e83053e41b4b1ab0429b
Fee <fee.braun@hotmail.com>
2012-12-11 22:26:21 +0100

    recursion 2

--- 121211_LabExercise10/src/Triangle.java
+++ 121211_LabExercise10/src/Triangle.java
@@ -34,21 +34,29 @@ public class Triangle {
 		}
 	}
 
-	public int drawRecursive(int xPointA, int xPointB, int xPointC,
+	public void drawRecursive(int xPointA, int xPointB, int xPointC,
 			int yPointA, int yPointB, int yPointC) {
-		if (step == 10) {
-			return 1;
-		} else {
+		while (step != 5){
 			int[] newXPoints = { (xPointA + xPointB) / 2, (xPointB + xPointC) / 2, (xPointC + xPointA) / 2 };
 			int[] newYPoints = { (yPointA + yPointB) / 2, (yPointB + yPointC) / 2, (yPointC + yPointA) / 2 };
-			if (step < 5) {
+			int[] newTopXPoints = {(xPointA + newXPoints[0])/2, (newXPoints[0] + newXPoints[2]) /2, (newXPoints[2] + xPointA) /2};
+			int[] newTopYPoints = {(yPointA + newYPoints[0])/2, (newYPoints[0] + newYPoints[2]) /2, (newYPoints[2] + yPointA) /2};
+			int[] newRightXPoints = {(newXPoints[0] + xPointB) /2, (xPointB + newXPoints[1]) /2, (newXPoints[1] + newXPoints[0]) /2};
+			int[] newRightYPoints = {(newYPoints[0] + yPointB) /2, (yPointB + newYPoints[1]) /2, (newYPoints[1] + newYPoints[0]) /2};
+			int[] newLeftXPoints = {(newXPoints[1] + newXPoints[2]) /2, (newXPoints[1] + xPointC) /2, (xPointC + newXPoints[2]) /2};
+			int[] newLeftYPoints = {(newYPoints[1] + newYPoints[2]) /2, (newYPoints[1] + yPointC) /2, (yPointC + newYPoints[2]) /2};
+			
+			if (step%2 == 0) {
 				changeColor("black");
 			} else {
 				changeColor("white");
 			}
 			canvas.draw(this, color, new Polygon(newXPoints, newYPoints, 3));
+			canvas.draw(this, color, new Polygon(newTopXPoints, newTopYPoints, 3));
+			canvas.draw(this, color, new Polygon(newRightXPoints, newRightYPoints, 3));
+			canvas.draw(this, color, new Polygon(newLeftXPoints, newLeftYPoints, 3));
 			step = step + 1;
-			return drawRecursive(newXPoints[0], newXPoints[1], newXPoints[2], newYPoints[0], newYPoints[1], newYPoints[2]);
+			drawRecursive(newXPoints[0], newXPoints[1], newXPoints[2], newYPoints[0], newYPoints[1], newYPoints[2]);
 		}
 	}
 }
