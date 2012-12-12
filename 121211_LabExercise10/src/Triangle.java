b442232fd28e94b9e47d95a917a3e80157d2d939
Fee <fee.braun@hotmail.com>
2012-12-12 15:57:46 +0100

    -...

--- 121211_LabExercise10/src/Triangle.java
+++ 121211_LabExercise10/src/Triangle.java
@@ -11,6 +11,38 @@ public class Triangle {
 	private Canvas canvas = new Canvas(screenWidth, screenHeigth, Color.white);
 	private int step;
 
+	public int getScreenWidth() {
+		return screenWidth;
+	}
+
+	public void setScreenWidth(int screenWidth) {
+		this.screenWidth = screenWidth;
+	}
+
+	public int getScreenHeigth() {
+		return screenHeigth;
+	}
+
+	public void setScreenHeigth(int screenHeigth) {
+		this.screenHeigth = screenHeigth;
+	}
+
+	public int getxPosition() {
+		return xPosition;
+	}
+
+	public void setxPosition(int xPosition) {
+		this.xPosition = xPosition;
+	}
+
+	public int getyPosition() {
+		return yPosition;
+	}
+
+	public void setyPosition(int yPosition) {
+		this.yPosition = yPosition;
+	}
+
 	/**
 	 * Create a new triangle at default position with default color.
 	 */
@@ -25,15 +57,17 @@ public class Triangle {
 		color = newColor;
 	}
 
-	public void draw() {
+	public void draw(int xPointA, int xPointB, int xPointC, int yPointA, int yPointB, int yPointC) {
 		if (isVisible) {
-			int[] xPoints = { xPosition + screenWidth / 2, (screenWidth - screenHeigth) / 2, ((screenWidth - screenHeigth) / 2) + screenHeigth };
-			int[] yPoints = { yPosition, screenHeigth, screenHeigth };
+			int[] xPoints = { xPointA, xPointB, xPointC };
+			int[] yPoints = { yPointA, yPointB, yPointC };
 			canvas.draw(this, color, new Polygon(xPoints, yPoints, 3));
+			
 			int[] newXPoints = { (xPoints[0] + xPoints[1]) / 2, (xPoints[1] + xPoints[2]) / 2, (xPoints[2] + xPoints[0]) / 2 };
 			int[] newYPoints = { (yPoints[0] + yPoints[1]) / 2, (yPoints[1] + yPoints[2]) / 2, (yPoints[2] + yPoints[0]) / 2 };
 			changeColor("Black");
 			canvas.draw(this, color, new Polygon(newXPoints, newYPoints, 3));
+			
 			drawRecursive(newXPoints[0], newXPoints[1], newXPoints[2], newYPoints[0], newYPoints[1], newYPoints[2], xPoints[0], xPoints[1], xPoints[2], yPoints[0], yPoints[1], yPoints[2] );
 		}
 	}
@@ -56,14 +90,13 @@ public class Triangle {
 				changeColor("white");
 //			}
 			
-			//canvas.draw(this, color, new Polygon(newXPoints, newYPoints, 3));
 			canvas.draw(this, color, new Polygon(newTopXPoints, newTopYPoints, 3));
 			canvas.draw(this, color, new Polygon(newRightXPoints, newRightYPoints, 3));
 			canvas.draw(this, color, new Polygon(newLeftXPoints, newLeftYPoints, 3));
 			
 			step = step + 1;
 			
-			drawRecursive(newTopXPoints[0], newTopXPoints[1], newTopXPoints[2], newTopYPoints[0], newTopYPoints[1], newTopYPoints[2], xPointA, xPointB, xPointC, yPointA, yPointB, yPointC);
+//			draw(newTopXPoints[0], newTopXPoints[1], newTopXPoints[2], newTopYPoints[0], newTopYPoints[1], newTopYPoints[2]);
 		}
 	}
 }
--- 121211_LabExercise10/src/main.java
+++ 121211_LabExercise10/src/main.java
@@ -1,10 +1,11 @@
 import java.awt.Color;
+import java.io.ObjectInputStream.GetField;
 
 public class main {
 
 	public static void main(String[] args) {
 		Triangle mytriangle = new Triangle();
-		mytriangle.draw();
+		mytriangle.draw(mytriangle.getxPosition() + mytriangle.getScreenWidth() / 2, (mytriangle.getScreenWidth() - mytriangle.getScreenHeigth()) / 2, ((mytriangle.getScreenWidth() - mytriangle.getScreenHeigth()) / 2) + mytriangle.getScreenHeigth(), mytriangle.getyPosition(), mytriangle.getScreenHeigth(), mytriangle.getScreenHeigth() );
 	}
 
 }
