e452dc1285d620421cb20d33be1f64daf79e0750
Fee <fee.braun@hotmail.com>
2012-12-18 15:27:19 +0100

    matrix calss

--- 121218_LabExcercise11/src/Matrix.java
+++ 121218_LabExcercise11/src/Matrix.java
@@ -1,12 +1,28 @@
 
 public class Matrix {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	String [] nameVertices;
+	int numberOfEdges;
+	int row = nameVertices.length;
+	int collum = nameVertices.length;
+	int [] weightOfEdges;
+	int [] [] matrixArray;
+	
+	public Matrix(int row, int edges, String [] nameVertices, int [] weight) {
+		this.row = row;
+		this.collum = row;
+		this.numberOfEdges = edges;
+		this.nameVertices = nameVertices;
+		this.weightOfEdges = weight;
 	}
-
+	
+	public void fillMatrix(int [] weightOfEdges){
+		int count = 0;
+		for (int i = 0; i < this.row; i++){
+			for (int j = 0; j < this.collum; j++){
+				this.matrixArray[i][j]=weightOfEdges[count];
+				count++;
+			}
+		}
+	}
 }
