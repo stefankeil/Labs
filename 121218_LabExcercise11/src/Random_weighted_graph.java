c971ad968d43979c76a15246685fe1963622541d
Stefan Keil <an.stefan.keil@googlemail.com>
2012-12-20 11:52:36 +0100

    Added Random Number Class for vertices and edges

---
+++ 121218_LabExcercise11/src/Random_weighted_graph.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+
+
+public class Random_weighted_graph {
+	
+	private Random randomnumber = new Random();
+	private int vertices;
+	private int edges;
+	
+	
+	
+	Random_weighted_graph(int vertices, int edges){
+		
+		vertices = randomnumber.nextInt(26);
+		edges = randomnumber.nextInt(52);
+		
+	}
+		
+	private ArrayList randomVertices(int numberOf){
+		ArrayList<String> verticesList = new ArrayList<String>();
+        for (int i=0; i<numberOf;i++){
+        	int oneVertice = randomnumber.nextInt(200);
+        	String temp= String.valueOf((char) oneVertice);
+        	verticesList.add(temp);
+        }
+    return verticesList;
+
+        }
+	}
+
