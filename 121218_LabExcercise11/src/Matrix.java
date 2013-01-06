ead2a7308c2ae083d6c53e1aa939a32f58a17a84
Fee <fee.braun@hotmail.com>
2013-01-06 14:08:50 +0100

    Changes Matrix & Graph next Step: shortest and cheapest path

--- 121218_LabExcercise11/src/Matrix.java
+++ 121218_LabExcercise11/src/Matrix.java
@@ -2,46 +2,62 @@ import java.util.Random;
 
 public class Matrix {
 
-	String[] nameVertices;
-	int numberOfEdges;
-	int row;
-	int col;
-	int[] weightOfEdges;
+	String[] vertices;
+	int size;
 	int[][] matrixArray;
-	int i;
+	int position;
 
-	public Matrix(int row, int edges, String[] nameVertices, int[] weight) {
-
-		this.numberOfEdges = edges;
-		this.nameVertices = nameVertices;
-		this.weightOfEdges = weight;
-		this.row = row;
-		this.col = row;
-		matrixArray = new int[row][row];
-
+	public Matrix(int size) {
+		this.size = size;
+		this.vertices = new String[size];
+		this.matrixArray = new int[size][size];
 	}
 
-	public void addVertices(String vertices) {
-		nameVertices[i] = vertices;
-		this.row = nameVertices.length;
-		this.col = nameVertices.length;
-		i++;
-	}
-
-	public void addWeight() {
-		Random randomnumber = new Random();
-		int rndRow = 0;
-		int rndCol = 0;
-
-		for (int z = 0; z < weightOfEdges.length; z++) {
-			rndRow = randomnumber.nextInt(row);
-			rndCol = randomnumber.nextInt(row);
-			if (matrixArray[rndRow][rndCol] == 0) {
-				matrixArray[rndRow][rndCol] = weightOfEdges[z];
-			} else {
-				z--;
-			}
-
+	public void addVertex(String vertex) throws Exception {
+		if (position < size) {
+			vertices[this.position] = vertex;
+			this.position++;
+		} else {
+			throw new Exception("Matrix to small");
 		}
+		
+	}
+
+	public void addWeight(String vertexA, String vertexB, int weight) throws Exception {
+		
+		int indexVertexA = vertexIndex(vertexA);
+		int indexVertexB = vertexIndex(vertexB);
+		
+		matrixArray[indexVertexA][indexVertexB] = weight;
+	
+		
+		
+		//		Random randomnumber = new Random();
+//		int rndRow = 0;
+//		int rndCol = 0;
+//
+//		for (int z = 0; z < weightOfEdges.length; z++) {
+//			rndRow = randomnumber.nextInt(row);
+//			rndCol = randomnumber.nextInt(row);
+//			if (matrixArray[rndRow][rndCol] == 0) {
+//				matrixArray[rndRow][rndCol] = weightOfEdges[z];
+//			} else {
+//				z--;
+//			}
+//
+//		}
+	}
+	
+	private int vertexIndex(String vertex) throws Exception {
+		for (int i = 0; i < this.vertices.length; i++) {
+			if (vertices[i].equals(vertex)) {
+				return i;
+			}
+		} 
+		throw new Exception("Unknown vertex " + vertex);
+	}
+	
+	public String[] getVertices() {
+		return vertices;
 	}
 }
