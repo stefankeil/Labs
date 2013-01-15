9958daa80c9586d9a9ddbb3a4a43559c18042718
Fee <fee.braun@hotmail.com>
2013-01-15 14:30:04 +0100

    fixed

--- 130115_LabExcercise13 /src/HashMap.java
+++ 130115_LabExcercise13 /src/HashMap.java
@@ -4,7 +4,7 @@ public class HashMap {
 
 	public void setWordInMap(int key, String word) {
 		int position = 0;
-		
+
 		while (scrabbleValues[key][position] != null) {
 			position++;
 		}
@@ -13,47 +13,47 @@ public class HashMap {
 
 	public String[] getWordListOnPosition(int hashCode) throws Exception {
 		int position = 0;
-		
+
 		while (scrabbleValues[hashCode][position] != null) {
 			position++;
 		}
-		
+
 		String[] wordArray = new String[position];
-		
-		for (int i = 0; i < position; i++){
+
+		for (int i = 0; i < position; i++) {
 			wordArray[i] = scrabbleValues[hashCode][i];
 		}
-		
+
 		return wordArray;
 	}
-	
+
 	public void figures() {
 		int nullArray = 0;
 		int bigCollision = 0;
-		for(int i = 0; i < scrabbleValues.length; i ++) {
+		for (int i = 0; i < scrabbleValues.length; i++) {
 			int collision = 0;
 			for (int j = 0; j < scrabbleValues[i].length; j++) {
-				if(scrabbleValues[i][j] != null){
+				if (scrabbleValues[i][j] != null) {
 					collision++;
 				} else {
 					break;
 				}
 			}
-//			System.out.println("index" + i + " Collision: " + collision);
-			if (bigCollision < collision){
+			// System.out.println("index" + i + " Collision: " + collision);
+			if (bigCollision < collision) {
 				bigCollision = collision;
 			}
-			if (collision == 0){
+			if (collision == 0) {
 				nullArray++;
 			}
 		}
 		// System.out.println("Keine Belegung: " + nullArray);
 		// System.out.println("Grš§te Kollision: " + bigCollision);
 	}
-	
+
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
-		for(int i = 0; i < scrabbleValues.length; i ++) {
+		for (int i = 0; i < scrabbleValues.length; i++) {
 			for (int j = 0; j < scrabbleValues[i].length; j++) {
 				sb.append(scrabbleValues[i][j]).append(" ");
 			}
--- 130115_LabExcercise13 /src/Main.java
+++ 130115_LabExcercise13 /src/Main.java
@@ -8,22 +8,30 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		cheater = new ScrabbleCheater(
-				"/Users/Fee/Documents/Uni/HTW/Informatik2/Lab/130115_LabExcercise13/TWL06.txt");
+				"/Users/Fee/Documents/Uni/HTW/Informatik2/Lab/130108_LabExcercise12/TWL06.txt");
 
 		String word = generateRandomString(7);
+		cheater.permute("", word);
+		
 		String words[] = cheater.getWords(word);
 
-		cheater.permute("", word);
-	}
+//		for (int i = 0; i < words.length; i++) {
+//			if (cheater.isPermutation(word, words[i])) {
+////				System.out.println(words[i]);
+//			} 
+		}
+		
+		private static String generateRandomString(int len) {
 
-	private static String generateRandomString(int len) {
+			Random rnd = new Random();
 
-		Random rnd = new Random();
+			StringBuilder sb = new StringBuilder(len);
+			for (int i = 0; i < len; i++) {
+				sb.append(AB.charAt(rnd.nextInt(AB.length())));
 
-		StringBuilder sb = new StringBuilder(len);
-		for (int i = 0; i < len; i++)
-			sb.append(AB.charAt(rnd.nextInt(AB.length())));
-		return sb.toString();
-
+		}
+			System.out.println("String" + sb.toString());
+			return sb.toString();
+		
 	}
 }
