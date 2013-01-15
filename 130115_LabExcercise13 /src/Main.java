ca378555da2c2cc3ad1b3b32cc2b7c59f1065639
Fee <fee.braun@hotmail.com>
2013-01-15 14:21:10 +0100

    permutation

--- 130115_LabExcercise13 /src/Main.java
+++ 130115_LabExcercise13 /src/Main.java
@@ -8,43 +8,22 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		cheater = new ScrabbleCheater(
-				"/Users/stefankeil/Documents/Workspace/Labs/130115_LabExcercise13/TWL06.txt");
+				"/Users/Fee/Documents/Uni/HTW/Informatik2/Lab/130115_LabExcercise13/TWL06.txt");
 
 		String word = generateRandomString(7);
 		String words[] = cheater.getWords(word);
 
-		for (int i = 0; i < words.length; i++) {
-			if (isPermutation(word, words[i])) {
-				System.out.println(words[i]);
-			} 
-		}
+		cheater.permute("", word);
 	}
 
-	private static String sortByAlphabet(String s) {
-		// sort them by alphabet
-		char[] content = s.toCharArray();
-		java.util.Arrays.sort(content);
-		String sorted = new String(content);
-		return sorted;
-	}
+	private static String generateRandomString(int len) {
 
-	private static boolean isPermutation(String a, String b) {
-		String sortedA = sortByAlphabet(a);
-//		System.out.println(a);
-		String sortedB = sortByAlphabet(b);
-//		System.out.println(b);
-		return (sortedA.compareTo(sortedB) == 0);
-	}
-	
-	
-	private static String generateRandomString(int len){
-	
-	Random rnd = new Random();
+		Random rnd = new Random();
 
-	StringBuilder sb = new StringBuilder( len );
-	for( int i = 0; i < len; i++ ) 
-		sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
-	return sb.toString();
-	}
+		StringBuilder sb = new StringBuilder(len);
+		for (int i = 0; i < len; i++)
+			sb.append(AB.charAt(rnd.nextInt(AB.length())));
+		return sb.toString();
 
+	}
 }
--- 130115_LabExcercise13 /src/ScrabbleCheater.java
+++ 130115_LabExcercise13 /src/ScrabbleCheater.java
@@ -3,18 +3,46 @@ public class ScrabbleCheater {
 	HashMap hashMap;
 
 	public ScrabbleCheater(String fileName) throws Exception {
-		hashMap = Reader
-				.readMapFromFile("/Users/stefankeil/Documents/Workspace/Labs/130115_LabExcercise13/TWL06.txt");
+		hashMap = Reader.readMapFromFile(fileName);
 		hashMap.figures();
-		String words[] = hashMap.getWordListOnPosition(16);
-		for (int i = 0; i < words.length; i++) {
-			// System.out.println(words[i]);
-		}
 	}
 
 	public String[] getWords(String string) throws Exception {
 		int hashInteger = Hashing.createHashcode(string);
 		return hashMap.getWordListOnPosition(hashInteger);
+	}
+
+	public static String sortByAlphabet(String s) {
+		// sort them by alphabet
+		char[] content = s.toCharArray();
+		java.util.Arrays.sort(content);
+		String sorted = new String(content);
+		return sorted;
+	}
+
+	public boolean isPermutation(String a, String b) {
+		String sortedA = sortByAlphabet(a);
+		String sortedB = sortByAlphabet(b);
+		return (sortedA.compareTo(sortedB) == 0);
+	}
+
+	public static void permute(String beginningString, String endingString) {
+		//Base Case
+		if (endingString.length() <= 1) {
+			System.out.println(beginningString + endingString);
+		} else {
+			for (int i = 0; i < endingString.length(); i++) {
+				try {
+					//newString will be the new endingString
+					//Substring make a substring from 0 to i and from i + 1
+					String newString = endingString.substring(0, i) + endingString.substring(i + 1);
+					permute(beginningString + endingString.charAt(i), newString);
+					
+				} catch (StringIndexOutOfBoundsException exception) {
+					exception.printStackTrace();
+				}
+			}
+		}
 	}
 
 }
